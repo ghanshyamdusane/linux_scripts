595d8a1db2fd7626397efe9e23bf12f1c16778e5
Ghanshyam Dusane <37286886+ghanshyamdusane@users.noreply.github.com>
2019-01-10 00:21:31 +0530

    Create loadalert.sh

---
+++ loadalert.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+    EMAIL="dusanegs@gmail.com"
+
+    EMAIL1="ghanshyam.dusane@capgemini.com"
+
+    SUBJECT="Alert $(hostname) Disk Space & Memory Status"
+
+    TEMPFILE="/tmp/$(hostname)"
+
+    echo "Hostname: $(hostname)" >> $TEMPFILE
+
+    echo "Local Date & Time : $(date)" >> $TEMPFILE
+
+    echo "-------------------------------------------" >> $TEMPFILE
+
+    echo "| Disk Space information : |" >> $TEMPFILE
+
+    echo "-------------------------------------------" >> $TEMPFILE
+
+    /bin/df -h >> $TEMPFILE
+
+    echo "-------------------------------------------" >> $TEMPFILE
+
+    echo "-------------------------------------------" >> $TEMPFILE
+
+    echo "| Memory and Swap status: |" >> $TEMPFILE
+
+    echo "-------------------------------------------" >> $TEMPFILE
+
+    /usr/bin/free -m >> $TEMPFILE
+
+    echo "-------------------------------------------" >> $TEMPFILE
+
+    mail -s "$SUBJECT" "$EMAIL" < $TEMPFILE
+
+    mail -s "$SUBJECT" "$EMAIL1" < $TEMPFILE
+
+    rm -f $TEMPFILE
