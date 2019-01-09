fc730b81d603859722edfbc427c48d44e0a15144
Ghanshyam Dusane <37286886+ghanshyamdusane@users.noreply.github.com>
2019-01-10 00:23:23 +0530

    Create autolog_clear.sh

---
+++ autolog_clear.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+check=$(df -h | grep -E " /" | head -n1 | cut -d "%" -f1 | awk '{print $4}')
+if [[ $check -gt 75 ]]; then
+echo > /var/log/messages
+echo > /var/log/haproxy.log
+echo "`date +%c`: Disk used% is $check , clearing logs" >> /var/log/clearlog.log
+fi
