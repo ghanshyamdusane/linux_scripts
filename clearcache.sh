fca540f96f5d935d8c1c8212ca29d777469bdf3a
Ghanshyam Dusane <37286886+ghanshyamdusane@users.noreply.github.com>
2019-10-01 18:47:43 +0530

    Update clearcache.sh

--- clearcache.sh
+++ clearcache.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Author and Maintained by : Ghanshyam Dusane
 # To Clear Page cache whenever Memory Utilization hits to 70%
-#Variables
 
-ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
-if [[ $ramusage -gt 70 ]]; then
+ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}' | cut -d "." -f1)
+if [ $ramusage -gt 70 ]; then
 echo 1 > /proc/sys/vm/drop_caches
 echo "`date +%c`: Memory used% is $ramusage , clearing cache" >> /var/log/clearcache.log
+else
+echo "memory Usage is $ramusage"
 fi
